c47a34be071f8f4f3ace0316633485957106797b
Masterchef365 <duncan.freeman1@gmail.com>
2023-06-03 17:50:27 -0700

    Im confused

--- src/lib.rs
+++ src/lib.rs
@@ -13,16 +13,22 @@ use rhai::{Dynamic, AST};
 // All state associated with client-side behaviour
 struct ClientState {
     ui: UiStateHelper,
-    rhai_engine: rhai::Engine,
-    rhai_scope: rhai::Scope<'static>,
+    engine: rhai::Engine,
+    scope: rhai::Scope<'static>,
     widget: UiHandle,
-    current_ast: AST,
+    script_ast: AST,
     response_text: String,
+    command: Option<String>,
 }
 
-const DEFAULT_SCRIPT: &str = "fn update() {
+const DEFAULT_SCRIPT: &str = r#"fn update() {
     
-}";
+}
+
+fn run_me() {
+    print("Hello, world!");
+}
+"#;
 
 impl UserState for ClientState {
     // Implement a constructor
@@ -35,12 +41,18 @@ impl UserState for ClientState {
         // Create chat "window"
         let schema = vec![
             Schema::TextBox,
-            //Schema::Button { text: "Run".into() },
+            Schema::TextInput,
+            Schema::Button { text: "Run".into() },
             Schema::Label,
         ];
         let state = vec![
-            State::TextBox { text: DEFAULT_SCRIPT.into() },
-            //State::Button { clicked: false },
+            State::TextBox {
+                text: DEFAULT_SCRIPT.into(),
+            },
+            State::TextInput {
+                text: "run_me()".into(),
+            },
+            State::Button { clicked: false },
             State::Label { text: "".into() },
         ];
         let widget = ui.add(io, "Rhai", schema, state);
@@ -63,11 +75,12 @@ impl UserState for ClientState {
         let rhai_scope = rhai::Scope::new();
 
         Self {
-            rhai_engine,
-            rhai_scope,
+            command: None,
+            engine: rhai_engine,
+            scope: rhai_scope,
             widget,
             ui,
-            current_ast: AST::default(),
+            script_ast: AST::default(),
             response_text: "".into(),
         }
     }
@@ -75,6 +88,7 @@ impl UserState for ClientState {
 
 impl ClientState {
     fn transform_editor(&mut self, io: &mut EngineIo, query: &mut QueryResult) {
+        // Copy ECS data into rhai
         let map: HashMap<String, Transform> = query
             .iter("Transforms")
             .map(|id @ EntityId(num)| (num.to_string(), query.read::<Transform>(id)))
@@ -82,24 +96,36 @@ impl ClientState {
 
         let rhai_dyn_map = rhai::serde::to_dynamic(&map).unwrap();
 
-        self.rhai_scope.push_dynamic("transforms", rhai_dyn_map);
+        self.scope.push_dynamic("transforms", rhai_dyn_map);
 
-        //let result = self.rhai_engine
-        //.run_ast_with_scope(&mut self.rhai_scope, &self.current_ast);
-
-        let result =
-            self.rhai_engine
-                .call_fn::<()>(&mut self.rhai_scope, &self.current_ast, "update", ());
+        // Run update() function in script
+        let result = self
+            .engine
+            .eval_ast_with_scope::<()>(&mut self.scope, &self.script_ast);
 
         if let Err(e) = result {
-            self.response_text = format!("Runtime error: {:#}", e);
-        } else {
-            let returned_map = self.rhai_scope.get("transforms").unwrap();
-            let ret_map: HashMap<String, Transform> = rhai::serde::from_dynamic(returned_map).unwrap();
-            for (key, value) in ret_map {
-                let ent = EntityId(key.parse().unwrap());
-                query.write(ent, &value);
-            }
+            self.response_text = format!("Error running update(): {:#}", e);
+        }
+
+        // Run any command line commands
+        if let Some(command) = self.command.take() {
+            let result = self
+                .engine
+                .eval_with_scope::<Dynamic>(&mut self.scope, &command);
+
+            self.response_text = match result {
+                Ok(result) => format!("Command: {}", result),
+                Err(e) => format!("Command caused error: {}", e),
+            };
+            dbg!(&self.response_text);
+        }
+
+        // Copy ECS data back into cimvr
+        let returned_map = self.scope.get("transforms").unwrap();
+        let ret_map: HashMap<String, Transform> = rhai::serde::from_dynamic(returned_map).unwrap();
+        for (key, value) in ret_map {
+            let ent = EntityId(key.parse().unwrap());
+            query.write(ent, &value);
         }
     }
 
@@ -109,23 +135,38 @@ impl ClientState {
 
         // Check for UI updates
         if io.inbox::<UiUpdate>().next().is_some() {
-            // Read the text input
+            // Compile the script
             let ui_state = self.ui.read(self.widget);
             let State::TextBox { text } = &ui_state[0] else { panic!() };
-            let result = self.rhai_engine.compile(text);
+            let script_compile_result = self.engine.compile(text);
 
-            self.response_text = match result {
+            self.response_text = match script_compile_result {
                 Ok(ast) => {
-                    self.current_ast = ast;
+                    self.script_ast = ast;
                     format!("Compilation successful")
                 }
                 Err(e) => format!("Compile error: {:#}", e),
             };
+
+            // Set the command line
+            if ui_state[2] == (State::Button { clicked: true }) {
+                let State::TextInput { text } = &ui_state[1] else { panic!() };
+                //let cmd_compile_result = self.engine.compile_expression(text);
+                self.command = Some(text.clone());
+
+                /*
+                match cmd_compile_result {
+                    Ok(ast) => {
+                    }
+                    Err(e) => self.response_text = format!("Commandline error: {:#}", e),
+                };
+                */
+            }
         }
 
-        // Clear the text input
+        // Set the response text
         self.ui.modify(io, self.widget, |states| {
-            states[1] = State::Label {
+            states[3] = State::Label {
                 text: self.response_text.clone(),
             };
         });
